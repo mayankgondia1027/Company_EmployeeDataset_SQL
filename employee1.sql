b4934a7974d8b1432eeaa544857cd57c59ba212a
Mayank Gondia <mayankgondia18@gmail.com>
2025-09-30 22:24:36 +0530

    Add files via upload

---
+++ employee1.sql
@@ -0,0 +1,33 @@
+create database google;
+use google;
+CREATE TABLE employee1 (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(10),
+    department_name VARCHAR(50)
+);
+
+
+INSERT INTO employee1 (employee_id, first_name, last_name, gender, department_name) VALUES
+(101, 'John', 'Doe', 'Male', 'Engineering'),
+(102, 'Jane', 'Smith', 'Female', 'Human Resources'),
+(103, 'Robert', 'Johnson', 'Male', 'Finance'),
+(104, 'Emily', 'Davis', 'Female', 'Marketing'),
+(105, 'Michael', 'Brown', 'Male', 'IT'),
+(106, 'Sophia', 'Wilson', 'Female', 'Engineering'),
+(107, 'David', 'Martinez', 'Male', 'Sales'),
+(108, 'Olivia', 'Garcia', 'Female', 'Finance'),
+(109, 'James', 'Anderson', 'Male', 'Operations'),
+(110, 'Isabella', 'Taylor', 'Female', 'Research');
+
+select*from employee1;
+
+
+
+
+
+
+
+
+
